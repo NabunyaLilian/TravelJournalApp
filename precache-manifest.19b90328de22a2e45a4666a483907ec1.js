e3e88b2904fb42207f352280e2c3e9b63623f333
Nabunya Lilian <liliannabunya2@gmail.com>
2022-06-07 19:09:43 +0300

    Updates

---
+++ precache-manifest.19b90328de22a2e45a4666a483907ec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "49366baf650cab85964e",
+    "url": "/static/css/main.7ed693bb.chunk.css"
+  },
+  {
+    "revision": "49366baf650cab85964e",
+    "url": "/static/js/main.49366baf.chunk.js"
+  },
+  {
+    "revision": "a37dc6f7b25f40a0edec",
+    "url": "/static/js/1.a37dc6f7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "81f08cf58d1ae5f976f1811178d8b8f5",
+    "url": "/index.html"
+  }
+];
